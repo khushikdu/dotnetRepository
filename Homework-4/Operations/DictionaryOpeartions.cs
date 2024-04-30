efb0ab396c78e6b09d2186123b772ffe7fe070cb
khushikdu <khushi.rani@kickdrumtech.com>
2024-04-30 23:02:32 +0530

    Adding files for Homework-4

---
+++ Homework-4/Operations/DictionaryOpeartions.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_4.Operations
+{
+    internal class DictionaryOpeartions
+    {
+        private Dictionary<int, double> dictionaryItems = new Dictionary<int, double>(){
+            { 1, 1 },
+            { 2,2 },
+            { 3, 3 },
+            { 4, 4 },
+            { 5, 5 }
+        };
+
+        
+        public void Operations() {
+            ListOperations listOperations = new ListOperations();
+            while (true)
+            {
+                listOperations.PrintMenu("Dictionary");
+                Console.Write("Enter your choice : ");
+                if (!int.TryParse(Console.ReadLine(), out int choice))
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+                    continue;
+                }
+                switch(choice)
+                {
+                    case 1:
+                        AddElement();
+                        break;
+                    case 2:
+                        PrintElements();
+                        break;
+                    case 3:
+                        DeleteElement(0,"First");
+                        break;
+                    case 4:
+                        int middleIndex=(dictionaryItems.Count%2==0)? dictionaryItems.Count / 2-1: dictionaryItems.Count / 2;
+                        DeleteElement(middleIndex, "Middle");
+                        break;
+                    case 5:
+                        DeleteElement(dictionaryItems.Count-1, "Last");
+                        break;
+                    case 6:
+                        CalculateAverage();
+                        break;
+                    case 7:
+                        return;
+                        break;
+                    case 8:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input. Please enter a valid number.");
+                        break;
+                }
+            }
+        }
+        public void AddElement()
+        {
+            Console.Write("Enter the key : ");
+            if (int.TryParse(Console.ReadLine(), out int key))
+            {
+                Console.Write("Enter the value : ");
+                if (double.TryParse(Console.ReadLine(), out double value))
+                {
+                    dictionaryItems[key] = value;
+                    Console.WriteLine("Element added successfully.");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid key. Please enter a valid integer key.");
+            }
+        }
+
+        public void PrintElements()
+        {
+            Console.WriteLine("Dictionary Items");
+            foreach(var item in dictionaryItems)
+            {
+                Console.WriteLine($"Key: {item.Key}, Value: {item.Value}");
+            }
+        }
+        public void DeleteElement(int index, string position)
+        {
+            if (dictionaryItems.Count > 0)
+            {
+                if (index >= 0 && index < dictionaryItems.Count)
+                {
+                    int keyToRemove = dictionaryItems.ElementAt(index).Key;
+                    dictionaryItems.Remove(keyToRemove);
+                    Console.WriteLine($"{position} item deleted successfully");
+                }
+                else
+                {
+                    Console.WriteLine($"Invalid index. {position} element does not exist.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Dictionary is empty");
+            }
+        }
+        public void CalculateAverage()
+        {
+            if (dictionaryItems.Count > 0)
+            {
+                double sum = 0;
+                int count = 0;
+                foreach (var value in dictionaryItems.Values)
+                {
+                    
+                        sum += value;
+                        count++;
+                    
+                }
+
+                if (count > 0)
+                {
+                    double average = sum / count;
+                    Console.WriteLine($"Average : {average}");
+                }
+                else
+                {
+                    Console.WriteLine("No valid integer values found in the dictionary.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Dictionary is empty");
+            }
+        }
+
+    }
+}
---
+++ Homework-4/Operations/ListOperations.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_4.Operations
+{
+    internal class ListOperations
+    {
+        private List<int> listItems = new List<int>();
+        public void PrintMenu(String collection)
+        {
+            Console.WriteLine($"\nPlease enter a number from the given options :");
+            Console.WriteLine($"1. Add an element to the {collection}.");
+            Console.WriteLine($"2. Print all the elements present in the {collection}.");
+            Console.WriteLine($"3. Delete the first element from the {collection}.");
+            Console.WriteLine($"4. Delete the middle element from the {collection}.");
+            Console.WriteLine($"5. Delete the last element from the {collection}.");
+            Console.WriteLine($"6. Calculate the average of the elements present in the {collection}.");
+            Console.WriteLine($"7. Go back to the previous menu.");
+            Console.WriteLine($"8. Exit the application.");
+        }
+        public void Operations()
+        {
+            while (true)
+            {
+                PrintMenu("List");
+                Console.Write("Enter your choice : ");
+                if (!int.TryParse(Console.ReadLine(), out int choice))
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+                    continue;
+                }
+                switch (choice)
+                {
+                    case 1:
+                        AddElement();
+                        break;
+                    case 2:
+                        PrintElements();
+                        break;
+                    case 3:
+                        DeleteElement(0, "First");
+                        break;
+                    case 4:
+                        DeleteElement(listItems.Count / 2, "Middle");
+                        break;
+                    case 5:
+                        DeleteElement(listItems.Count-1, "Last");
+                        break;
+                    case 6:
+                        CalculateAverage();
+                        break;
+                    case 7:
+                        return;
+                        break;
+                    case 8:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input. Please enter a valid number.");
+                        break;
+                }
+            }
+        }
+        public void AddElement()
+        {
+            Console.Write("Enter the number to be added : ");
+            if (int.TryParse(Console.ReadLine(), out int value))
+            {
+                listItems.Add(value);
+                Console.WriteLine("Item added successfully");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input");
+            }
+        }
+        public void PrintElements()
+        {
+            Console.WriteLine("List Items");
+            foreach (int items in listItems)
+            {
+                Console.WriteLine(items);
+            }
+        }
+        public void DeleteElement(int index, string position)
+        {
+            if (listItems.Count > 0)
+            {
+                listItems.RemoveAt(index);
+                Console.WriteLine($"{position} item deleted successfully");
+            }
+            else
+            {
+                Console.WriteLine("List is empty");
+            }
+        }
+        public void CalculateAverage()
+        {
+            if (listItems.Count > 0)
+            {
+                double average = listItems.Average();
+                Console.WriteLine($"Average : {average}");
+            }
+            else
+            {
+                Console.WriteLine("List is empty");
+            }
+        }
+    }
+}
