db66ce471f3892ffa02fb8349fc5508a637f431b
khushikdu <khushi.rani@kickdrumtech.com>
2024-05-02 01:20:23 +0530

    Adding Docs

--- Program.cs
+++ Program.cs
@@ -2,6 +2,9 @@
 
 namespace Homework3
 {
+    /// <summary>
+    /// Represents a logger for logging messages to a file.
+    /// </summary>
     class Logger : IDisposable
     {
         private static Logger instance;
@@ -9,12 +12,19 @@ namespace Homework3
         private StreamWriter _streamWriter;
         private bool disposed = false;
 
+        /// <summary>
+        /// Initializes a new instance of the Logger class with the specified log file path.
+        /// </summary>
+        /// <param name="logFilePath">The path of the log file.</param>
         private Logger(string logFilePath)
         {
             this._logFilePath = string.IsNullOrWhiteSpace(logFilePath) ? "log.txt" : logFilePath;
             this._streamWriter = File.AppendText(_logFilePath);
         }
 
+        /// <summary>
+        /// Gets the singleton instance of the Logger class.
+        /// </summary>
         public static Logger Instance
         {
             get
@@ -27,6 +37,10 @@ namespace Homework3
             }
         }
 
+        /// <summary>
+        /// Logs a message with the current timestamp.
+        /// </summary>
+        /// <param name="message">The message to be logged.</param>
         private void Log(string message)
         {
             string directory = Path.GetDirectoryName(_logFilePath);
@@ -37,12 +51,19 @@ namespace Homework3
             _streamWriter.WriteLine($"{DateTime.Now} : {message}");   
         }
 
+        /// <summary>
+        /// Disposes the logger and releases any resources used.
+        /// </summary>
         public void Dispose()
         {
             Dispose(true);
             GC.SuppressFinalize(this);
         }
 
+        /// <summary>
+        /// Disposes the logger and releases any resources used.
+        /// </summary>
+        /// <param name="disposing">A boolean value indicating whether to dispose managed resources.</param>
         protected virtual void Dispose(bool disposing)
         {
             if (!disposed)
@@ -55,11 +76,18 @@ namespace Homework3
             }
         }
 
+        /// <summary>
+        /// Finalizes the logger instance.
+        /// </summary>
         ~Logger()
         {
             Dispose(false);
         }
 
+        /// <summary>
+        /// Main method to log a message.
+        /// </summary>
+        /// <param name="args">Command-line arguments.</param>
         public static void Main(string[] args)
         {
             Console.WriteLine($"Enter message to be logged : ");
@@ -68,9 +96,13 @@ namespace Homework3
             Console.WriteLine("Message logged successfully");
             Logger.Instance.Dispose();
             Console.WriteLine("Disposed status: " + Logger.Instance.IsDisposed());
-            //Console.ReadKey();
+            Console.ReadKey();
         }
 
+        /// <summary>
+        /// Checks if the logger has been disposed.
+        /// </summary>
+        /// <returns>True if the logger has been disposed; otherwise, false.</returns>
         public bool IsDisposed()
         {
             return disposed;
