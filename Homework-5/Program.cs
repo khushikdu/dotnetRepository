4de7e254de01564aa9d1bacdd971ce88864a7307
khushikdu <khushi.rani@kickdrumtech.com>
2024-05-01 22:33:38 +0530

    Adding files for homework-5

---
+++ Homework-5/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Homework_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Enumerable.Range(1, 10).ToList();
+
+            List<int> squaredNumbers = numbers.Select(x => x * x).ToList();
+
+            List<int> evenNumbers = numbers.Where(x => x % 2 == 0).ToList();
+
+            int sumOfEvenNumbers = evenNumbers.Sum();
+
+            Console.WriteLine("Original List:");
+            PrintList(numbers);
+
+            Console.WriteLine("\nSquared List:");
+            PrintList(squaredNumbers);
+
+            Console.WriteLine("\nFiltered List (Even Numbers Only):");
+            PrintList(evenNumbers);
+
+            Console.WriteLine($"\nSum of Even Numbers: {sumOfEvenNumbers}");
+        }
+
+        static void PrintList(List<int> list)
+        {
+            foreach (var item in list)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+        }
+    
+    }
+}
