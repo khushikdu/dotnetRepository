3ec156bb394c718f12066d42f762ea71e330ef80
khushikdu <khushi.rani@kickdrumtech.com>
2024-05-08 21:35:28 +0530

    Optimizing Validations

--- Homework-6/Model/StudentRequestDTO.cs
+++ Homework-6/Model/StudentRequestDTO.cs
@@ -1,4 +1,6 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace Homework_6.Model
 {
@@ -10,11 +12,15 @@ namespace Homework_6.Model
         /// <summary>
         /// Gets or sets the name of the student.
         /// </summary>
+        [Required(ErrorMessage = "Name is a required field")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Name must be between 3 to 50 characters")]
         public string Name { get; set; }
 
         /// <summary>
         /// Gets or sets the age of the student.
         /// </summary>
+        [Required(ErrorMessage = "Age is a required field.")]
+        [Range(13, 25, ErrorMessage = "Age must be between 13 and 25.")]
         public int Age { get; set; }
 
         /// <summary>
